994f5db9cc248a1413d6391fcd18f042d11fb111
Daniel Tan <danieltan380@gmail.com>
2023-10-11 12:57:46 -0700

    Change tweetData into an array of test tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,24 +3,37 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
   const $tweet = createTweetElement(tweetData);
-$('#tweets-container').append($tweet);
+  $("#tweets-container").append($tweet);
 });
 
-let tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+let tweetData = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-  "created_at": 1461116232227
-};
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
-const createTweetElement = function(tweet) {
+const createTweetElement = function (tweet) {
   const { name, handle, avatars } = tweet.user;
   const { text } = tweet.content;
   const dateCreated = tweet.created_at;
@@ -43,5 +56,5 @@ const createTweetElement = function(tweet) {
       </div>
     </footer>
   </article>
-  `)
+  `);
 };
