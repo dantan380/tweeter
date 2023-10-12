6c77890d7468d393f4a321f372ac2c36b7f8a392
Daniel Tan <danieltan380@gmail.com>
2023-10-11 20:05:51 -0700

    Implement displayError function. Move loadTweets function and loadTweets function call before newTweet submission. Add code to empty tweets-container when submitting new code, before loading tweets to avoid duplicate tweets. Add code to reset character counter upon submitting new tweet.Add escape function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  $("#newTweet").submit(function (event) {
-    event.preventDefault();
 
-    const formData = $(this).serialize();
-
-    const tweetText = $('#tweet-text').val();
-
-    const clearTweetTextArea = $('#tweet-text').val('');
-
-    if (tweetText === "" || tweetText === null) {
-      alert(
-        "Tweet cannot be empty. Please type something before clicking submit."
-      );
-    } else if (tweetText.length > 140) {
-      alert(
-        "Tweet content exceeds 140 characters. Please submit a shorter tweet."
-      );
-    } else {
-      $.post("/tweets", formData)
-        .done(function (response) {
-          console.log("Tweet has been submitted", response);
-          loadTweets();
-          clearTweetTextArea;
-        })
-        .fail(function (xhr, status, error) {
-          console.log("Error:", status, error);
-        });
-    }
-  });
+  const displayError = function (errorMessage) {
+    const $errorElement = $('.error-message');
+    $errorElement.text(errorMessage);
+    $errorElement.slideDown();
+  };
 
   const loadTweets = function () {
     $.ajax({
@@ -50,9 +27,54 @@ $(document).ready(function () {
   };
 
   loadTweets();
+
+  $("#newTweet").submit(function (event) {
+    event.preventDefault();
+
+    const formData = $(this).serialize();
+
+    const tweetText = $('#tweet-text').val();
+
+    const clearTweetTextArea = $('#tweet-text').val('');
+
+    if (tweetText === "" || tweetText === null) {
+
+      const error_message = "Tweet cannot be empty. Please type something before clicking submit."
+      displayError(error_message);
+      $('#tweet-text').on('focus', function() {
+        $('.error-message').slideUp();
+      });
+
+    } else if (tweetText.length > 140) {
+      const error_message = "Tweet content exceeds 140 characters. Please submit a shorter tweet."
+      displayError(error_message);
+      $('#tweet-text').on('focus', function() {
+        $('.error-message').slideUp();
+      });
+
+    } else {
+      $.post("/tweets", formData)
+        .done(function (response) {
+          console.log("Tweet has been submitted", response);
+          $("#tweets-container").empty();
+          $('.counter').html(140);
+          loadTweets();
+          clearTweetTextArea;
+        })
+        .fail(function (xhr, status, error) {
+          console.log("Error:", status, error);
+        });
+    }
+  });
 });
 
 const createTweetElement = function (tweet) {
+// Escape function to prevent users from typing HTML that will affect the web app.
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
   const { name, handle, avatars } = tweet.user;
   const { text } = tweet.content;
   const dateCreated = tweet.created_at;
@@ -65,7 +87,7 @@ const createTweetElement = function (tweet) {
       </div>
       <span class="account">${handle}</span>
     </header>
-    <p class="tweet-text">${text}</p>
+    <p class="tweet-text">${escape(text)}</p>
     <footer class="tweet-footer">
       ${timeago.format(dateCreated)}
       <div class="icons">
