78e234cb7575bbcc196b46b37f376bb243517950
Daniel Tan <danieltan380@gmail.com>
2023-10-11 17:11:28 -0700

    Implement checks to make sure user is not entering an empty tweet or a tweet that exceeds 140 characters

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,39 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-
   $("#newTweet").submit(function (event) {
     event.preventDefault();
 
     const formData = $(this).serialize();
 
-    $.post("/tweets", formData)
-      .done(function (response) {
-        console.log("Tweet has been submitted", response);
-      })
-      .fail(function (xhr, status, error) {
-        console.log("Error:", status, error);
-      });
+    const tweetText = $('#tweet-text').val();
+
+    const clearTweetTextArea = $('#tweet-text').val('');
+
+    if (tweetText === "" || tweetText === null) {
+      alert(
+        "Tweet cannot be empty. Please type something before clicking submit."
+      );
+    } else if (tweetText.length > 140) {
+      alert(
+        "Tweet content exceeds 140 characters. Please submit a shorter tweet."
+      );
+    } else {
+      $.post("/tweets", formData)
+        .done(function (response) {
+          console.log("Tweet has been submitted", response);
+          loadTweets();
+          clearTweetTextArea;
+        })
+        .fail(function (xhr, status, error) {
+          console.log("Error:", status, error);
+        });
+    }
   });
 
-  const loadTweets = function() {
+  const loadTweets = function () {
     $.ajax({
-      url:'/tweets',
-      method: 'GET',
-      dataType: 'json',
-      success: function(response) {
-        console.log("response", response)
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
+        console.log("response", response);
         renderTweets(response);
       },
-      error: function(error) {
+      error: function (error) {
         console.log("Error message", error);
-      }
+      },
     });
-  }
+  };
 
   loadTweets();
 });
-
 
 const createTweetElement = function (tweet) {
   const { name, handle, avatars } = tweet.user;
